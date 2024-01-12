af100184bcc411dea43b4f49e99a1c457becb299
Saigafarova_A <saigafarova00@yandex.ru>
2024-01-12 21:52:31 +0300

    feat: add aliases and scss jest support

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,6 +5,12 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@assets/(.*)$": "<rootDir>/src/assets/$1",
+    "\\.(css|scss)$": "identity-obj-proxy"
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
   },
