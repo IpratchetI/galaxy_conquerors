8ce3fb97ced15d2c77f53d856ad2d436ec46c0ac
Saigafarova_A <saigafarova00@yandex.ru>
2024-01-13 22:22:12 +0300

    feat: add topics in forum page

---
+++ packages/client/src/Pages/Forum/ForumPage.tsx
@@ -0,0 +1,12 @@
+import s from './ForumPage.module.scss'
+import { TOPICS_LIST } from './lib/constants'
+import { TopicsList } from './components/TopicsList/TopicsList'
+
+export const ForumPage = () => {
+	return (
+		<div className={s.Page}>
+			<h2 className={s.Title}>Forum</h2>
+			<TopicsList topics={TOPICS_LIST} />
+		</div>
+	)
+}
---
+++ packages/client/src/Pages/Forum/components/TopicsList/TopicsList.tsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react'
+import { Topics } from '@models/types/topics'
+import s from './TopicsList.module.scss'
+import { Link } from '@components/Link/Link'
+import { TopicsHeader } from '../TopicsHeader/TopicsHeader'
+import { abbreviateNumber } from '@/utils/abbreviateNumber'
+
+type TopicsListProps = {
+	topics?: Topics
+}
+
+export const TopicsList = ({ topics }: TopicsListProps) => {
+	const handleClick = useCallback(() => {
+		// todo: add handler
+	}, [])
+
+	return (
+		<>
+			<TopicsHeader className={s.Topic} />
+			<ul className={s.TopicsList} aria-label="forum-topics-list">
+				{topics?.map(topic => (
+					<li key={topic.id} className={s.TopicItem} title={topic.name}>
+						<Link
+							href={`/forum/${topic.id}`}
+							params={{
+								topicName: topic.name,
+							}}
+							className={s.Topic}
+							onClick={handleClick}>
+							<span className={s.TopicName}>{topic.name}</span>
+							<span>{abbreviateNumber(topic.comments, 0)}</span>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+}
