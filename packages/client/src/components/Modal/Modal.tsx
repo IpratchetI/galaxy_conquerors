353a27aa08ee51162a1728d6660f18ee622ebc65
Saigafarova_A <saigafarova00@yandex.ru>
2024-01-14 17:19:59 +0300

    feat: add modal component

---
+++ packages/client/src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import s from './Modal.module.scss';
+import { ReactNode } from 'react';
+import { Portal } from './components/Portal/Portal';
+import { Overlay } from './components/Overlay/Overlay';
+
+type ModalProps = {
+	className?: string;
+	isOpen?: boolean;
+	onClose?: () => void;
+	onOpen?: () => void;
+	children?: ReactNode;
+};
+
+export const Modal = (props: ModalProps) => {
+	const { className, children, isOpen, onClose } = props;
+
+	const mods = {
+		[s.Opened]: isOpen,
+	};
+
+	return (
+		<Portal>
+			<div className={classNames(s.Modal, mods)}>
+				<Overlay onClick={onClose} isOpen={isOpen} />
+				<div className={classNames(s.Content, [className])}>{children}</div>
+			</div>
+		</Portal>
+	);
+};
---
+++ packages/client/src/components/Modal/components/Overlay/Overlay.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import classnames from 'classnames';
+import s from './Overlay.module.scss';
+
+interface OverlayProps {
+	onClick?: () => void;
+	isOpen?: boolean;
+}
+
+export const Overlay = (props: OverlayProps) => {
+	const { onClick, isOpen } = props;
+	const [openedState, setOpenedState] = useState(false);
+
+	useEffect(() => {
+		if (isOpen === undefined) return;
+		if (isOpen) {
+			const timeoutID = setTimeout(() => {
+				setOpenedState(true);
+			}, 400);
+			return () => clearTimeout(timeoutID);
+		}
+		setOpenedState(false);
+	});
+
+	return (
+		<div
+			onClick={onClick}
+			className={classnames(s.Overlay, { [s.Opened]: openedState })}
+		/>
+	);
+};
