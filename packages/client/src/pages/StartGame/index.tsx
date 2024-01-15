643c51b87d1086ae27c673b7fd90e0853e53ae8f
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-01-15 23:35:19 +0300

    add background

--- packages/client/src/pages/StartGame/index.tsx
+++ packages/client/src/pages/StartGame/index.tsx
@@ -1,10 +1,14 @@
 import styles from './index.module.scss'
 import astronaut from '@assets/img/astronaut.png'
+import { Spacer } from '@/components'
 
 const StartGame = () => {
 	return (
 		<main className={styles.background}>
 			<img src={astronaut} className={styles.astronaut} />
+			<Spacer className={styles.textContainer}>
+				Над планетой U-571 нависла угроза...
+			</Spacer>
 		</main>
 	)
 }
