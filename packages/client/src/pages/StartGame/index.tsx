d85756d410e858c5503d331139d2972b45b83605
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-01-17 23:42:59 +0300

    fixed lint

--- packages/client/src/pages/StartGame/index.tsx
+++ packages/client/src/pages/StartGame/index.tsx
@@ -1,21 +1,20 @@
-import styles from './index.module.scss'
-import astronaut from '@assets/img/astronaut.png'
-import { Spacer, Text } from '@/components'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import astronaut from '@assets/img/astronaut.png';
+
+import { Spacer, Text } from '@/components';
+
+import styles from './index.module.scss';
 
 const StartGame = () => {
 	useEffect(() => {
 		//TODO добавить роут
 		//setTimeout(() => {}, 5000)
-	}, [])
+	}, []);
 
 	return (
 		<main className={styles.background}>
 			<img src={astronaut} className={styles.astronaut} />
-			<Spacer
-				direction="column"
-				align="start"
-				className={styles.cloudContainer}>
+			<Spacer direction="column" align="start" className={styles.cloudContainer}>
 				<div className={styles.cloudMin}></div>
 				<div className={styles.cloudMax}></div>
 				<div className={styles.textContainer}>
@@ -25,7 +24,7 @@ const StartGame = () => {
 				</div>
 			</Spacer>
 		</main>
-	)
-}
+	);
+};
 
-export default StartGame
+export default StartGame;
