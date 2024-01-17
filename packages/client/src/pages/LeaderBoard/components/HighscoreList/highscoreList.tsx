1ea8d1f84174ac43c9dfe5eee1ddcf486b7381f7
Alexander Tsarkov <pratchet1989@yandex.ru>
2024-01-17 13:19:13 +0300

    wip

--- packages/client/src/pages/LeaderBoard/components/HighscoreList/highscoreList.tsx
+++ packages/client/src/pages/LeaderBoard/components/HighscoreList/highscoreList.tsx
@@ -1,4 +1,3 @@
-// highscoreList.tsx
 import React from 'react'
 
 const mockHighscores = [
@@ -7,7 +6,7 @@ const mockHighscores = [
 	{ username: '#playerName3', score: 1900 },
 	{ username: '#playerName4', score: 800 },
 	{ username: '#playerName5', score: 500 }
-	// Другие моковые данные
+	// todo:заменить моковые данные на данные из бэка
 ]
 
 export const HighscoreList: React.FC = () => {
