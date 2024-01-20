59239a7cd49b4e00561507e6c6e107815051cb6f
Alexander Tsarkov <pratchet1989@yandex.ru>
2024-01-20 23:13:42 +0300

    fix conflicts

--- packages/client/src/pages/LeaderBoard/index.tsx
+++ packages/client/src/pages/LeaderBoard/index.tsx
@@ -15,7 +15,7 @@ export const LeaderBoard: React.FC = () => {
 			<div className={s.highscoreList}>
 				<HighscoreList />
 			</div>
-			<Button className={s.button} title="Back" onClick={handleHistoryBack}></Button>
+			<Button title="Back" className={s.button} onClick={handleHistoryBack} />
 		</div>
 	);
 };
