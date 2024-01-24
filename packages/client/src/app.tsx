74caa0fb648ca8998c8f3e125d5122d1199016a0
Alexander Tsarkov <pratchet1989@yandex.ru>
2024-01-24 17:44:49 +0300

    fix

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorPage } from '@pages/Error';
-import Game from '@pages/Game';
+// import Game from '@pages/Game';
 import { AppRouter } from '@components/AppRouter/AppRouter';
 
 import './app.css';
