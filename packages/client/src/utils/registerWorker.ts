11a91d685eb04a2ccc3a24edca8a1d928bcc33d3
Saigafarova_A <saigafarova00@yandex.ru>
2024-02-17 03:16:12 +0300

    fix: minor

--- packages/client/src/utils/registerWorker.ts
+++ packages/client/src/utils/registerWorker.ts
@@ -12,7 +12,7 @@ export function unregisterServiceWorker() {
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker.ready.then(reg => {
 			reg.unregister();
-			console.info('ServiceWorker register');
+			console.info('ServiceWorker unregistered');
 		});
 	} else {
 		console.warn('ServiceWorker unregistration failed');
