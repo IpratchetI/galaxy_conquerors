c4a05ee25a1f0cb003c1ca81179ccef21d41e013
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-02-23 11:22:24 +0300

    fixed test

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-var, vars-on-top */
 import type { TPreloadedState } from 'src/store/store';
 
-declare const __SERVER_PORT__: number;
 declare module '*.scss';
 declare module '*.svg';
 declare module '*.png';
@@ -11,4 +10,5 @@ export {};
 
 declare global {
 	var __PRELOADED_STATE__: TPreloadedState;
+	const __SERVER_PORT__: number;
 }
