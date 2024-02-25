68da1b651a9f6306745ce8e5df33b0561e05f0e9
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-02-25 12:04:04 +0300

    fixed var

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -9,6 +9,6 @@ declare module '*.test.[ts|tsx]';
 export {};
 
 declare global {
-	var __PRELOADED_STATE__: TPreloadedState;
+	let __PRELOADED_STATE__: TPreloadedState;
 	const __SERVER_PORT__: number;
 }
