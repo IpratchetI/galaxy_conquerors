11ff6d89b8862853781cc0ae1d037587f86ae7a9
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-03-08 11:59:54 +0300

    fixed app.ts

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -2,13 +2,8 @@ import { useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorPage } from '@pages/Error';
 import { AppRouter } from '@components/AppRouter/AppRouter';
-import { useAppDispatch } from './store';
-import { getTopicsList } from './store/reducers/forum/forumReducer';
-import { TOPICS_LIST } from '@pages/Forum/lib/mocks';
 
 function App() {
-	const dispatch = useAppDispatch();
-
 	useEffect(() => {
 		const fetchServerData = async () => {
 			const url = `http://localhost:${__SERVER_PORT__}`;
@@ -18,8 +13,6 @@ function App() {
 		};
 
 		fetchServerData();
-
-		dispatch(getTopicsList(TOPICS_LIST));
 	}, []);
 
 	return (
